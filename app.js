e5eb271a82c0a3da3baf9f4a59da72328e7faff4
Faizan Khalid <businessmy060@gmail.com>
2025-04-05 09:36:08 +0500

    Updated features section and added accordion script

--- app.js
+++
@@ -1,4 +0,0 @@
-    function downloadNova() {
-      // Replace the link below with your actual GitHub download link
-      window.open('https://github.com/NextGenCodde/Nova-AI-Voice-Assistant/releases/download/v1.0/app.exe';
-    }
